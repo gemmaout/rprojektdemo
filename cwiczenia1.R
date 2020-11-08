10d5f446d0fc289fa439973dc7de93391a4d0f54
Szulakowska <aszulakowska@microstrategy.com>
2020-11-08 09:33:31 +0100

    poprawki do kodu

--- cwiczenia1.R
+++ cwiczenia1.R
@@ -4,7 +4,10 @@ library("devtools")
 install.packages("httr")
 install.packages("jsonlite")
 
-endpoint<-"api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
+endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
 getWeather<-GET(endpoint)
 
-writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+weatherText<-content(getWeather,"text")
+weatherJson<-fromJSON(weatherText,flatten = TRUE)
+weatherDF<-as.data.frame(weatherJson)
+View(weatherDF)
