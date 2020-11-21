fb75f82441f7b48209de9fd787b3f9c5936cc4b2
Szulakowska <aszulakowska@microstrategy.com>
2020-11-21 16:04:33 +0100

    New API example

--- cwiczenia1.R
+++ cwiczenia1.R
@@ -11,3 +11,12 @@ weatherText<-content(getWeather,"text")
 weatherJson<-fromJSON(weatherText,flatten = TRUE)
 weatherDF<-as.data.frame(weatherJson)
 View(weatherDF)
+
+
+endpoint2<-"http://api.open-notify.org/astros.json"
+
+getSpace<-GET(endpoint2)
+spaceText<-content(getSpace,"text")
+spaceJson<-fromJSON(spaceText,flatten = TRUE)
+spaceDF<-as.data.frame(spaceJson)
+View(spaceDF)
